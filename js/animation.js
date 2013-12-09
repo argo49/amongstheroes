f523184eee946a52479493adb58dd32337a83ed0
argo49 <argo_49@hotmail.com>
2013-12-08 21:55:06 -0500

    Moons spin and stop on mouseover

--- js/animation.js
+++ js/animation.js
@@ -37,7 +37,100 @@ function square(context, x, y, size, color){
 	context.fill();
 }
 
+var numberOfMoons = 8;
+
+// Changes the CSS of each moon to move vertically and horizontally a set amount of degrees
+function drawMoons(canvasX, canvasY, canvasWidth, canvasHeight, degrees){
+	// Draws moons on top of planet in moonCanvas
+	var centerPlanetX = canvasX + (canvasWidth  * 0.285),
+	    centerPlanetY = canvasY + (canvasHeight * 0.49),
+	    moons    	  = [];
+
+	// Populate array with jqQuery moon handles
+    for(var i = 0; i < numberOfMoons; i++){
+    	var moonImg = $('#moon' + i);
+    	moons.push(moonImg);
+    }
+
+    // Orbit radius of the moons
+    var orbitRadius = (0.15 * canvasWidth) + (moons[0].height() / 2);
+
+    // Returns the new coordinates in a css object based on the degrees provided
+    function moonCoords(degrees){
+    	// http://stackoverflow.com/questions/20168357/stuttering-orbit-animation-when-updating-css-through-javascript-over-small-inter
+		return {left:centerPlanetX + (orbitRadius * Math.cos((degrees*Math.PI)/180)), 
+			  	top :centerPlanetY - (orbitRadius * Math.sin((degrees*Math.PI)/180))};
+	}
+
+	// Adds a set amount of degrees to the rotation of the moon
+	function addDegrees(moon, degreeIncrement){
+		var newAngle = moon.data('angle') + degreeIncrement;	
+		moon.data('angle', newAngle);
+
+		return moonCoords(newAngle);
+	}
+
+	// Change css of the moons
+	for(var i = 0; i < numberOfMoons; i++){
+		moons[i].css(addDegrees(moons[i], degrees), 1);
+	}
+
+}
+
+// Create the visible moon imgs and set them in place with event handlers
+function createMoons(canvasX, canvasY, canvasWidth, canvasHeight){
+	var centerPlanetX = canvasX + (canvasWidth  * 0.285),
+	    centerPlanetY = canvasY + (canvasHeight * 0.49),
+	    moons         = [],
+	    content       = $('#content');
+
+	// Populate an array with jquery moon objects
+	for(var i = 0; i < numberOfMoons; i++){
+		var moonImg = $('#moon' + i)
+			.addClass('moon')
+			.on('mouseover', function(){
+				if(moonInterval){
+				    clearInterval(moonInterval);
+				    moonInterval = undefined;
+				}
+			})
+			.on('mouseout', function(){
+				// Breaks on window resize since it's using the old values
+				if(!moonInterval)
+					setMoonInterval(canvasX, canvasY, canvasWidth, canvasHeight);
+			})
+			.height(canvasHeight * 0.08)
+			.show();
+
+		moonImg.appendTo(content);
+		moons.push(moonImg);
+	}
+
+	// Radius of the moons' orbit
+	var orbitRadius = (0.15 * canvasWidth) + (moons[0].height() / 2);
+
+	// Returns a css object that should be used with jQuery.css() to place moons based on degrees
+	function moonCoords(degrees){
+		return {left:centerPlanetX + (orbitRadius * Math.cos((degrees*Math.PI)/180)), 
+			  	top :centerPlanetY - (orbitRadius * Math.sin((degrees*Math.PI)/180))};
+	}
+
+	// Position of first moon in degrees (full moon)
+	var zeroMoon = 90;
+
+	// Set each moon in place based on it's position in degrees
+	for(var i = 0; i < numberOfMoons; i++){
+		moons[i].css(moonCoords(zeroMoon));
+		moons[i].data("angle", zeroMoon);
+		zeroMoon -= 45;
+	}
+
+	return true;
+}
+
+
 var moonInterval;
+var moonsSet;
 
 function draw(){
 	var canvas = $('#canvas')[0];
@@ -45,44 +138,35 @@ function draw(){
 	var ctx = canvas.getContext('2d');
 	var canvasWidth  = jqCanvas.attr('width');
 	var canvasHeight = jqCanvas.attr('height');
-	var canvasX = Number(jqCanvas.css('left').substring(0,jqCanvas.css('left').length-2));
-	var canvasY = Number(jqCanvas.css('top').substring(0,jqCanvas.css('top').length-2));
+	var canvasX = Number(jqCanvas.css('left').slice(0, -2));
+	var canvasY = Number(jqCanvas.css('top').slice(0, -2));
 
 	//draw the background image to fill canvas
 	var backgroundImage = document.getElementById('background');
 	ctx.drawImage(backgroundImage, 0, 0, canvasWidth, canvasHeight);
 
+	// If the moons are not set in position, set them
+	if(!moonsSet){
+		moonsSet = createMoons(canvasX, canvasY, canvasWidth, canvasHeight);
+	}
+
 	//make moons rotate sensually
+	setMoonInterval(canvasX, canvasY, canvasWidth, canvasHeight);
+}
+
+// Move the moons vertically and horizontaly a small amount every iteration
+function setMoonInterval(canvasX, canvasY, canvasWidth, canvasHeight){
 	if(!moonInterval){
 		moonInterval = window.setInterval(function(){
-			drawMoons(canvasX, canvasY, canvasWidth, canvasHeight);
-		}, 50);
+			drawMoons(canvasX, canvasY, canvasWidth, canvasHeight, -0.05);
+		}, 1);
 	}
 }
 
-
-var moonDegrees = 0;
-function drawMoons(canvasX, canvasY, canvasWidth, canvasHeight){
-	//draw moons on top of planet in moonCanvas
-	var moons = document.getElementById('moons');
-	var moonW = (canvasWidth / 2.3);
-	var moonH = (canvasWidth / 2.3);
-	var moonX = canvasX + (canvasWidth / 11);
-	var moonY = canvasY + (canvasHeight / 6);
-	var moonCanvas = $('#moonCanvas')
-		.attr('width', moonW)
-		.attr('height', moonH)
-		.css({
-			left:moonX,
-			top:moonY
-		});
-	var moonCtx = moonCanvas[0].getContext('2d');
-	moonCanvas.attr('width', moonW);
-	moonCanvas.attr('height', moonH);
-
-	//make the moons rotate slowly and sensually
-	moonDegrees += 0.2;
-	rotateCanvas(moonDegrees, moonCanvas[0], moons)
+function hideMoons(){
+	for(var i = 0; i < 8; i++){
+		$('#moon' + i).hide();
+	}
 }
 
 function rotateCanvas(degrees, canvas, imageToRotate){
@@ -157,8 +241,19 @@ function resize(){
 
 //initialization things
 function init(){
+	var fileNames = [
+		"background.png"
+		,"moon0.png"
+		,"moon1.png"
+		,"moon2.png"
+		,"moon3.png"
+		,"moon4.png"
+		,"moon5.png"
+		,"moon6.png"
+		,"moon7.png"
+	];
 	// store image names here
-	var imageSRCs = $.map(["background.png", "moons.png"], function(img, idx){
+	var imageSRCs = $.map(fileNames, function(img, idx){
 		return "images/" + img;
 	});
 	preloadImages(imageSRCs);
